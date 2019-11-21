57f2ae67ddcba2ccb4e19b4b1943c5698de4be0f
Maja G <37628684+mimikrija@users.noreply.github.com>
2019-11-21 20:31:54 +0100

    [temp] add Bows and Papers solution
    This is a standalone solution, todo: include it in the project.

---
+++ 02nd_Day.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+
+using namespace std;
+
+
+int main()
+{
+	std::string InputFileName;
+	cout << "Input file name? \n";
+	cin >> InputFileName;
+	ifstream InputFile(InputFileName);
+
+    // read input line per line
+
+    vector<string> ListOfInputs{ istream_iterator<string>{InputFile},{} };
+    
+    int TotalArea,TotalBowLength = 0;
+    // split at 'x' into integers length, width and height
+    for ( auto OneInput : ListOfInputs)
+    {
+        auto posxf = OneInput.find_first_of('x');
+        auto posxl = OneInput.find_last_of('x');
+        int l = stoi(OneInput.substr(0,posxf));
+        int h = stoi(OneInput.substr(posxl+1));
+        int w = stoi(OneInput.substr(posxf+1,posxl));
+        
+        vector Sides = {l,h,w};
+        sort(Sides.begin(),Sides.end());
+        
+        TotalArea += 2*l*w + 2*w*h + 2*h*l + Sides[0]*Sides[1];
+        
+        // second part
+        
+        TotalBowLength += 2*Sides[0] + 2*Sides[1] + l*h*w;
+
+        
+    }
+
+    cout << "The elves will need a total of " << TotalArea << " square feet of wrapping paper... \n";
+    cout << "..and a total of " << TotalBowLength << " feet of bow! \n";
+	return 0;
+}
