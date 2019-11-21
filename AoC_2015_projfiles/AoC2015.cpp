9d336d412397de4624e6ff16e850751cb46eb921
Maja Gacesa <maja.gacesa@ansys.com>
2019-11-21 17:40:03 +0100

    Create a proper solution (with funky structure)
    Might change it later, I am still trying to figure out the nicest
    way of keeping the source files. Anyway. I run AoC2015.cpp - this
    is the main(). From there, we call the input file and based on
    that, we call the source files in the root. I wanted to do it
    without the header files, but it is not possible (without putting
    everything in a single source file) - hence the headers.
    
    Also I am not sure if I need all the project files source controlled.

---
+++ AoC_2015_projfiles/AoC2015.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "..\01st_Day.h"
+#pragma once
+
+
+using namespace std;
+
+
+int main()
+{
+	std::string InputFileName;
+	cout << "Input file name? \n";
+	cin >> InputFileName;
+	ifstream InputFile(InputFileName);
+
+	// we can read that from the input file name
+	cout << "Which day of Christmas? \n";
+	int day;
+	cin >> day;
+	
+	switch(day)
+	{
+	case 1: 
+		Day_01(InputFile);
+	}
+
+	return 0;
+}
