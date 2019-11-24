616bde73fe8795c3df562b1cba742bab9eae0f90
Maja Gacesa <maja.gacesa@gmail.com>
2019-11-24 20:08:53 +0100

    add vowel check (the first nice criterion)

--- 05th_Day.cpp
+++ 05th_Day.cpp
@@ -8,9 +8,28 @@
 
 using namespace std;
 
+bool IsNice(string SantasString)
+{
+	// a nice string has to contain at least three vowels
+	int CountVowels(0);
+	for (auto Vowel : "aeiou")
+	{
+		if (find(SantasString.begin(),SantasString.end(),Vowel) != SantasString.end()) CountVowels++;
+		if (CountVowels >= 3) break;
+	}
+	if (CountVowels < 3) return false;
+
+}
+
 
 void Day_05(ifstream& InputFile)
 {
 
 	// read input line per line
+
+	vector<string> ListOfInputs{ istream_iterator<string>{InputFile},{} };
+
+	cout << "A number of nice strings is: " << count_if(ListOfInputs.begin(), ListOfInputs.end(), [](auto AString) {return IsNice(AString); }) << "!\n";
+
+
 }
