d7edeb7e467aba88fdefd95ba507198a06c7eeae
Maja Gacesa <maja.gacesa@gmail.com>
2019-11-25 20:29:20 +0100

    [day06] prep for day 6

---
+++ 06th_Day.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include "DayHeaders.h"
+
+using namespace std;
+
+
+void Day_06(ifstream& InputFile)
+{
+
+	// read input line per line
+	vector<string> ListOfInputs;
+	
+	string str;
+	while (getline(InputFile, str))
+	{
+		ListOfInputs.push_back(str);
+	}
+
+
+}
+
