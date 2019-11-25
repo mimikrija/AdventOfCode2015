fd2134f93ca2839d8028b12d46aa9aaa38e19bba
Maja <maja.gacesa@gmail.com>
2019-11-25 07:00:53 +0100

    add prep for part two

--- 05th_Day.cpp
+++ 05th_Day.cpp
@@ -33,8 +33,23 @@ void Day_05(ifstream& InputFile)
 	// read input line per line
 
 	vector<string> ListOfInputs{ istream_iterator<string>{InputFile},{} };
-
+	// part 1
 	cout << "The number of nice strings is: " << count_if(ListOfInputs.begin(), ListOfInputs.end(), [](auto AString) {return IsNice(AString); }) << "!\n";
 
+// PART 2
+
+	//-- - Part Two-- -
+	//	Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice.None of the old rules apply, as they are all clearly ridiculous.
+
+	//	Now, a nice string is one with all of the following properties :
+
+	//It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy(xy) or aabcdefgaa(aa), but not like aaa(aa, but it overlaps).
+	//	It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi(efe), or even aaa.
+	//	For example :
+
+	//qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice(qj) and a letter that repeats with exactly one letter between them(zxz).
+	//	xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+	//	uurcxstgmygtbstg is naughty because it has a pair(tg) but no repeat with a single letter between them.
+	//	ieodomkazucvgmuy is naughty because it has a repeating letter with one between(odo), but no pair that appears twice.
 
 }
