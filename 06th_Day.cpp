a91c5f8e729d7cf31d01ec9c66fc6c7b80960ccc
Maja Gacesa <maja.gacesa@gmail.com>
2019-11-26 07:55:17 +0100

    [day06] create coordinate generating function

--- 06th_Day.cpp
+++ 06th_Day.cpp
@@ -8,6 +8,19 @@
 
 using namespace std;
 
+vector<pair<int, int>> GeneratePairs(int x, int y, int X, int Y)
+{
+	vector<pair<int, int>> temp;
+	for (int horizontal = x; horizontal <= X; horizontal++)
+	{
+		for (int vertical = y; vertical <= Y; vertical++)
+		{
+			temp.push_back(make_pair(horizontal, vertical));
+		}
+	}
+	return temp;
+
+}
 
 void Day_06(ifstream& InputFile)
 {
