76ce5c9779252c73df11fd3fd50aafcad3bd3bfd
Maja Gacesa <maja.gacesa@gmail.com>
2020-11-22 14:05:41 +0100

    [day08] cleanup

--- 08th_Day.py
+++ 08th_Day.py
@@ -1,12 +1,12 @@
 total = 0
 strings = 0
-countlines = 0
 encoding = 0
+
 for lines in open('./inputs/input08','r'):
-    countlines += 1
     line = lines.split('\n')[0] # this is to ignore new line character from counting literals
     total += len(line)
     strings += len(eval(lines)) # eval is an inverse of repr (sort of), it interprets all the escape characters
+    # print ("literal string is: ", line, ". interpreted string is: ", eval(lines))
     # print (line, " length is ", len(line), "actual length is ", len(eval(lines)))
     temp = line.replace('"','\\')
     encoding += len(repr(temp))
