3dd8df89a2f2351bad14eac1a669b233a449023b
Maja Gacesa <maja.gacesa@gmail.com>
2020-11-22 13:58:46 +0100

    [day 08] improve second part using `repr`
    I tried `repr` initially, but I wasn't getting correct results in all
    cases. This is because `repr` encloses a string with single quotes `'`,
    and only escapes backslashes. Double quotes `"` do not get escaped.
    
    A workaround was to replace all quotes with backslashes. In that case
    `repr` inserts extra backslashes. Since we are interested in the count
    only, not in the actual encodig, this does the trick.

--- 08th_Day.py
+++ 08th_Day.py
@@ -1,33 +1,18 @@
 total = 0
 strings = 0
 countlines = 0
+encoding = 0
 for lines in open('./inputs/input08','r'):
     countlines += 1
     line = lines.split('\n')[0] # this is to ignore new line character from counting literals
     total += len(line)
     strings += len(eval(lines)) # eval is an inverse of repr (sort of), it interprets all the escape characters
     # print (line, " length is ", len(line), "actual length is ", len(eval(lines)))
+    temp = line.replace('"','\\')
+    encoding += len(repr(temp))
 
 print ("First part solution is: ", total - strings)
-# first part 1333
+# First part solution is:  1333
 
-infile = open('./inputs/input08','r')
-
-encoding = 0
-while 1:
-      
-    # read by character 
-    char = infile.read(1)
-    if not char:
-        break
-          
-    if char == '"' or char == '\\':
-        encoding += 2
-    elif char!='\n': # ignore new line!!
-        encoding += 1
-infile.close()
-
-encoding += countlines*2
-
-
-print("Second part solution is: ", encoding - total)
+print ("Second part solution is: ", encoding - total)
+# Second part solution is:  2046
