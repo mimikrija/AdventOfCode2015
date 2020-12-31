4856e877c6152b268cc2c72ee57861f3b8b69334
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-31 14:29:26 +0100

    [day01] separate parts 1 and two

--- 01.py
+++ 01.py
@@ -3,15 +3,17 @@ with open('inputs/input01') as input_file:
 
 go = lambda p: 1 if p == '(' else -1
 
+final_floor = sum(go(direction) for direction in inputs)
+
+print(f'The instructions take Santa to floor no. {final_floor}!')
+# The instructions take Santa to floor no. 74!
+
 current_floor = 0
-first_time_in_basement = None
 for pos, direction in enumerate(inputs):
     current_floor += go(direction)
-    if current_floor == -1 and first_time_in_basement == None:
+    if current_floor == -1:
         first_time_in_basement = pos + 1
-
-print(f'The instructions take Santa to floor no. {current_floor}!')
-# The instructions take Santa to floor no. 74!
+        break
 
 print(f'The first instruction for which Santa ends up in the basement is {first_time_in_basement}!')
 # The first instruction for which Santa ends up in the basement is 1795!
