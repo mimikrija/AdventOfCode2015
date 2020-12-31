a6ed9441e416820cad90b924150ea01e312d2ac6
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-31 15:00:16 +0100

    [day01] use meaningful variable names

--- 01.py
+++ 01.py
@@ -1,21 +1,21 @@
 with open('inputs/input01') as input_file:
-    inputs = input_file.read()
+    elevator_instructions = input_file.read()
 
 go = lambda p: 1 if p == '(' else -1
 
-final_floor = sum(go(direction) for direction in inputs)
+final_floor = sum(go(direction) for direction in elevator_instructions)
 
 print(f'The instructions take Santa to floor no. {final_floor}!')
 # The instructions take Santa to floor no. 74!
 
-def find_instruction(inputs, first_time_at_floor):
+def find_instruction(instructions, first_time_at_floor):
     current_floor = 0
-    for pos, direction in enumerate(inputs):
+    for pos, direction in enumerate(instructions):
         current_floor += go(direction)
         if current_floor == -1:
             return pos + 1
 
-first_time_in_basement = find_instruction(inputs, -1)
+first_time_in_basement = find_instruction(elevator_instructions, -1)
 
 print(f'The first instruction for which Santa ends up in the basement is {first_time_in_basement}!')
 # The first instruction for which Santa ends up in the basement is 1795!
