1da47231a7639d9bab48be6b6bfabc0ebd50912f
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-31 14:58:58 +0100

    [day01] move searching into a function

--- 01.py
+++ 01.py
@@ -8,12 +8,14 @@ final_floor = sum(go(direction) for direction in inputs)
 print(f'The instructions take Santa to floor no. {final_floor}!')
 # The instructions take Santa to floor no. 74!
 
-current_floor = 0
-for pos, direction in enumerate(inputs):
-    current_floor += go(direction)
-    if current_floor == -1:
-        first_time_in_basement = pos + 1
-        break
+def find_instruction(inputs, first_time_at_floor):
+    current_floor = 0
+    for pos, direction in enumerate(inputs):
+        current_floor += go(direction)
+        if current_floor == -1:
+            return pos + 1
+
+first_time_in_basement = find_instruction(inputs, -1)
 
 print(f'The first instruction for which Santa ends up in the basement is {first_time_in_basement}!')
 # The first instruction for which Santa ends up in the basement is 1795!
