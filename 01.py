5d8663ac28c25617e63b0dea82d68d215b83a66e
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-31 14:04:20 +0100

    [day01] part 1 - count parentheses

--- 01.py
+++ 01.py
@@ -1,2 +1,12 @@
+from collections import Counter
+
 with open('inputs/input01') as input_file:
     inputs = input_file.read()
+
+up_down_count = Counter(inputs)
+up = up_down_count['(']
+down = up_down_count[')']
+
+current_floor = up - down
+print(f'The instructions take Santa to floor no. {current_floor}!')
+# The instructions take Santa to floor no. 74!
