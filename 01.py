a235e7e2637efb1337c45f62a5ad95906f4b7fb5
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-31 14:26:29 +0100

    [day01] there is: solve both parts at once
    I don't like it because the first part could be solved via sum of
    gos.

--- 01.py
+++ 01.py
@@ -1,25 +1,17 @@
-from collections import Counter
-
 with open('inputs/input01') as input_file:
     inputs = input_file.read()
 
-up_down_count = Counter(inputs)
-up = up_down_count['(']
-down = up_down_count[')']
-
-current_floor = up - down
-print(f'The instructions take Santa to floor no. {current_floor}!')
-# The instructions take Santa to floor no. 74!
+go = lambda p: 1 if p == '(' else -1
 
 current_floor = 0
+first_time_in_basement = None
 for pos, direction in enumerate(inputs):
-    if direction == '(':
-        current_floor += 1
-    if direction == ')':
-        current_floor -= 1
-    if current_floor == -1:
+    current_floor += go(direction)
+    if current_floor == -1 and first_time_in_basement == None:
         first_time_in_basement = pos + 1
-        break
+
+print(f'The instructions take Santa to floor no. {current_floor}!')
+# The instructions take Santa to floor no. 74!
 
 print(f'The first instruction for which Santa ends up in the basement is {first_time_in_basement}!')
 # The first instruction for which Santa ends up in the basement is 1795!
