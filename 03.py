aa6efc9bea3bd4a0b779159c21d9236944f93ad1
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-31 20:23:23 +0100

    [day03] define first part as a function
    so that it simplifies part two solution

--- 03.py
+++ 03.py
@@ -8,13 +8,15 @@ DIRECTIONS = {
 with open('inputs/input03') as input_file:
     inputs = input_file.read().strip()
 
-house = 0 + 0j
-visited_houses = {house}
-for arrow in inputs:
-    house += DIRECTIONS[arrow]
-    visited_houses.add(house)
+def visit_houses(inputs):
+    house = 0 + 0j
+    visited_houses = {house}
+    for arrow in inputs:
+        house += DIRECTIONS[arrow]
+        visited_houses.add(house)
+    return visited_houses
 
-part_1 = len(visited_houses)
+part_1 = len(visit_houses(inputs))
 
 print(f'The number of houses which recieve at least one present is {part_1}!')
 # The number of houses which recieve at least one present is 2081!
