602bc61732b60a35d99999d5d8059ee2263d0dbd
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-31 15:02:48 +0100

    [day01] final cleanup and reshuffle

--- 01.py
+++ 01.py
@@ -1,12 +1,4 @@
-with open('inputs/input01') as input_file:
-    elevator_instructions = input_file.read()
-
 go = lambda p: 1 if p == '(' else -1
-
-final_floor = sum(go(direction) for direction in elevator_instructions)
-
-print(f'The instructions take Santa to floor no. {final_floor}!')
-# The instructions take Santa to floor no. 74!
 
 def find_instruction(instructions, first_time_at_floor):
     current_floor = 0
@@ -15,7 +7,15 @@ def find_instruction(instructions, first_time_at_floor):
         if current_floor == -1:
             return pos + 1
 
-first_time_in_basement = find_instruction(elevator_instructions, -1)
+with open('inputs/input01') as input_file:
+    elevator_instructions = input_file.read()
 
+# part 1
+final_floor = sum(go(direction) for direction in elevator_instructions)
+print(f'The instructions take Santa to floor no. {final_floor}!')
+# The instructions take Santa to floor no. 74!
+
+# part 2
+first_time_in_basement = find_instruction(elevator_instructions, -1)
 print(f'The first instruction for which Santa ends up in the basement is {first_time_in_basement}!')
 # The first instruction for which Santa ends up in the basement is 1795!
