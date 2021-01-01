616bcfc5bfff0ca32ab8182c6ca01ea0406660b8
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-01 18:30:42 +0100

    [day09][TIL] this is actually travelling salesman
    So for a small number of towns/stars (<20) it makes sense to just go
    through all possible combinations. This works if ALL nodes are connected
    (they are, and that was not my initial assumption when solving part 1
    with my version of Dijkstra).

--- 09.py
+++ 09.py
@@ -1,28 +1,11 @@
 # Day 9: All in a Single Night
+import itertools
 
-def distance_between(node_1, node_2, nodes_and_distances):
-    return nodes_and_distances[node_1][node_2]
-
-def neighbors(node, nodes_and_distances):
-    return set(nodes_and_distances[node].keys())
-
-def shortest_path(initial_node, nodes_and_distances):
-    visited_set = {initial_node}
-    unvisited_set = STARS - visited_set
-    current_node = initial_node
-    total_distance = 0
-    while unvisited_set:
-        neighbors_and_distances = ((neighbor, distance_between(current_node, neighbor, nodes_and_distances)) for neighbor in neighbors(current_node, nodes_and_distances) if neighbor not in visited_set)
-        neighbors_and_distances = sorted(neighbors_and_distances, key= lambda p: p[1])
-        visited, distance = neighbors_and_distances[0]
-        unvisited_set -= {visited}
-        visited_set |= {visited}
-        total_distance += distance
-        current_node = visited
-
-    return total_distance
-        
-
+def calculate_distance(combo, nodes_and_distances):
+    distance = 0
+    for first, second in zip(combo, combo[1:]):
+        distance += nodes_and_distances[first][second]
+    return distance
 
 
 with open('inputs/input09') as input_file:
@@ -45,6 +28,8 @@ for line in star_distances:
     STARS |= {first, second}
 
 
-all_distances = (shortest_path(star, stars_and_distances) for star in STARS)
-part_1 = min(all_distances)
-print(part_1) # 207
+
+all_distances = [calculate_distance(combo, stars_and_distances) for combo in itertools.permutations(STARS)]
+part_1, part_2 = min(all_distances), max(all_distances)
+print(f'Shortest distance is {part_1}, longest distance is {part_2}!')
+# Shortest distance is 207, longest distance is 804!
