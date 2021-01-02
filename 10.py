67434735e5f974a32a026b53eabdfe79afc7a595
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-02 15:02:37 +0100

    [day10] add nice printouts

--- 10.py
+++ 10.py
@@ -1,6 +1,6 @@
 # Day 10: Elves Look, Elves Say
 
-test = '1113122113'
+puzzle_input = '1113122113'
 
 def say_numbers(in_string):
     digit = in_string[0]
@@ -24,7 +24,9 @@ def play_the_elf_game(in_string, rep):
         result_string = say_numbers(result_string)
     return result_string
 
-part_1, part_2 = (len(play_the_elf_game(test, rounds)) for rounds in {40,50})
-print(part_1, part_2) # 360154 5103798
+for rounds in {40, 50}:
+    solution = len(play_the_elf_game(puzzle_input,rounds))
+    print(f'The length of the sequence after {rounds} rounds of the "Say numbers" game is: {solution}!')
 
-part_2 = 5103798 # 56th sum in this list https://oeis.org/A022471/b022471.txt https://oeis.org/A022471
+# The length of the sequence after 40 rounds of the "Say numbers" game is: 360154!
+# The length of the sequence after 50 rounds of the "Say numbers" game is: 5103798!
