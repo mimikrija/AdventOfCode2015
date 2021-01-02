384d6228d7fdaffc06a195836caa876da152938b
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-02 17:22:01 +0100

    [day11] first attempt not working
    I didn't take overlaps into account, so even though it seems that all
    conditions are satisfied for 'hxcaabcc' this is actually not true
    because both pairs 'aa' and 'cc' overlap with three conseq. letters
    'abc'.

--- 11.py
+++ 11.py
@@ -2,6 +2,21 @@
 
 inc_letter = lambda c: 97 if c == 122 else c + 1
 
+def increment_password(in_password):
+    remainder = 0
+    for pos in range(len(in_password)-1, -1, -1):
+        if ord(in_password[pos]) + remainder == 122:
+            new_value = 97
+            remainder = 1
+        else:
+            new_value = ord(in_password[pos]) + 1
+            remainder = 0
+        in_password[pos] = chr(new_value)
+        if remainder == 0:
+            return in_password
+
+
+
 def has_three_conseq_letters(password):
     for c in range(len(password)-2):
         if ord(password[c]) == ord(password[c+1]) - 1 == ord(password[c+2]) - 2:
@@ -28,3 +43,12 @@ def is_valid(password):
     status &= does_not_have_iol(password)
     status &= contains_two_pairs(password)
     return status
+
+my_input = list('hxbxwxba')
+
+new_password = list(my_input)
+
+while not is_valid(new_password):
+    new_password = increment_password(new_password)
+
+print("".join(c for c in new_password)) # not hxcaabcc
