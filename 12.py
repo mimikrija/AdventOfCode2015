f6bbbbce8eae847382cdc41cff232a594750dac8
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-03 16:07:44 +0100

    [day12] part 2

--- 12.py
+++ 12.py
@@ -1,20 +1,29 @@
 import json
 
-def get_int_items(in_item, out):
+def get_int_items(in_item, out, is_part_2 = False):
     for item in in_item:
         if isinstance(item, int):
             out.append(item)
         if isinstance(item, list):
-            get_int_items(item, out)
+            get_int_items(item, out, is_part_2)
         if isinstance(item, dict):
-            get_int_items(item.values(), out)
+            if any(key=='red' for key in list(item.keys())+list(item.values())) and is_part_2:
+                continue
+            else:
+                get_int_items(item.values(), out, is_part_2)
     return out
 
 dict_json = dict()
 with open('inputs/input12') as input_file:
     input_json = json.loads(input_file.read())
 
-solution = list()
-get_int_items(input_json, solution)
-part_1 = sum(item for item in solution)
-print(part_1) # 156366
+solution_1 = list()
+get_int_items(input_json, solution_1)
+part_1 = sum(solution_1)
+print(part_1) # 156366 # 96852 part 2
+
+solution_2 = list()
+get_int_items(input_json, solution_2, True)
+part_2 = sum(solution_2)
+print(part_2)
+
