af0555cf6bcf0bba57e0b43f6ab7694898b284a1
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-04 19:56:39 +0100

    [day16] parse aunt input

---
+++ 16.py
@@ -0,0 +1,30 @@
+# Day 16: Aunt Sue
+
+import re
+
+CORRECT_AUNT_SUE = {
+    'children': 3,
+    'cats': 7,
+    'samoyeds': 2,
+    'pomeranians': 3,
+    'akitas': 0,
+    'vizslas': 0,
+    'goldfish': 5,
+    'trees': 3,
+    'cars': 2,
+    'perfumes': 1,
+}
+
+re_words = re.compile(r'[a-z]+')
+re_numbers = re.compile(r'\d+')
+
+with open('inputs/input16') as input_file:
+    aunts_input = input_file.readlines()
+
+all_aunts = []
+for line in aunts_input:
+    aunt_keys = re.findall(re_words, line)[1:]
+    aunt_values = map(int, re.findall(re_numbers, line)[1:])
+    aunt = {aunt_key: aunt_value for aunt_key, aunt_value in zip(aunt_keys, aunt_values)}
+    all_aunts.append(aunt)
+
