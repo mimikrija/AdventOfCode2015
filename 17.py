014e157ba4e36f36636ded48cf76c15ac40aa7ca
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-05 11:36:44 +0100

    [day17] part 1 first go

--- 17.py
+++ 17.py
@@ -5,19 +5,16 @@ with open('inputs/input17') as input_file:
     input_containers = input_file.readlines()
 
 CONTAINERS = tuple(int(num) for num in input_containers)
-
 LITERS = 150
 #CONTAINERS = (5, 5, 10, 15, 20)
-FACTOR_RANGES = [range(0,2) for container in CONTAINERS]
-print(len(CONTAINERS))
 
-combos = (combo for combo in itertools.product(*FACTOR_RANGES))
+
+combos = (combo for combo in itertools.product({1,0}, repeat = len(CONTAINERS)))
 
 counter = 0
 for combo in combos:
     if sum(container * factor for container, factor in zip(CONTAINERS, combo)) == LITERS:
-        print(combo)
         counter += 1
 
 
-print(counter) # 2724 too high # 609 too low
+print(counter) # 1638
