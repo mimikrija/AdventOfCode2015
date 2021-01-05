df8930cde49fad4eb276c8be07a22f6ac90bd186
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-05 12:06:39 +0100

    [day17] refactor + part 2

--- 17.py
+++ 17.py
@@ -1,5 +1,6 @@
 # Day 17: No Such Thing as Too Much
 import itertools
+from collections import Counter
 
 with open('inputs/input17') as input_file:
     input_containers = input_file.readlines()
@@ -8,13 +9,13 @@ CONTAINERS = tuple(int(num) for num in input_containers)
 LITERS = 150
 #CONTAINERS = (5, 5, 10, 15, 20)
 
-
 combos = (combo for combo in itertools.product({1,0}, repeat = len(CONTAINERS)))
 
-counter = 0
-for combo in combos:
-    if sum(container * factor for container, factor in zip(CONTAINERS, combo)) == LITERS:
-        counter += 1
+all_valid_combos = [combo for combo in combos if sum(container * factor for container, factor in zip(CONTAINERS, combo)) == LITERS]
+part_1 = len(all_valid_combos)
+print(part_1)
 
-
-print(counter) # 1638
+min_num_of_containers_used = min(Counter(valid_combo)[1] for valid_combo in all_valid_combos)
+combos_with_min_number_of_containers = len([valid_combo for valid_combo in all_valid_combos if Counter(valid_combo)[1] == min_num_of_containers_used])
+part_2 = combos_with_min_number_of_containers
+print(part_2)
