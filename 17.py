512db778841d58fd210a5ecf21a86c9388ee460e
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-05 20:36:16 +0100

    [day18] part 1. we have a fixed mesh!
    Initially I implemented it with mesh expanding (like in 2020 day 17), so
    I needed to put a limit on the mesh dimensions when defining neighbors.
    
    It is pretty slow, I don't know what could be improved here, probably
    with a fixed mesh we can generate all the neighbors at once and maybe
    put that in a dictionary? Not sure if it would help.

--- 17.py
+++ 17.py
@@ -1,21 +1,29 @@
 # Day 17: No Such Thing as Too Much
 import itertools
-from collections import Counter
 
 with open('inputs/input17') as input_file:
     input_containers = input_file.readlines()
 
 CONTAINERS = tuple(int(num) for num in input_containers)
 LITERS = 150
+#LITERS = 25
 #CONTAINERS = (5, 5, 10, 15, 20)
 
-combos = (combo for combo in itertools.product({1,0}, repeat = len(CONTAINERS)))
-
-all_valid_combos = [combo for combo in combos if sum(container * factor for container, factor in zip(CONTAINERS, combo)) == LITERS]
+all_valid_combos = [combo for n in range(2, len(CONTAINERS)+1) for combo in itertools.combinations(CONTAINERS, n) if sum(combo) == LITERS]
 part_1 = len(all_valid_combos)
 print(part_1)
 
-min_num_of_containers_used = min(Counter(valid_combo)[1] for valid_combo in all_valid_combos)
-combos_with_min_number_of_containers = len([valid_combo for valid_combo in all_valid_combos if Counter(valid_combo)[1] == min_num_of_containers_used])
+# because of the way all_valid_combos is generated, it is actually already sorted on combo length
+min_num_of_containers_used = len(all_valid_combos[0])
+
+combos_with_min_number_of_containers = list(filter(lambda x: len(x) == min_num_of_containers_used, all_valid_combos))
+
+print(len(combos_with_min_number_of_containers))
+quit()
+
+print(all_valid_combos[0], all_valid_combos[15], all_valid_combos[-1])
+
+min_num_of_containers_used = min(len(valid_combo) for valid_combo in all_valid_combos)
+combos_with_min_number_of_containers = len([valid_combo for valid_combo in all_valid_combos if len(valid_combo) == min_num_of_containers_used])
 part_2 = combos_with_min_number_of_containers
 print(part_2)
--- 18.py
+++ 18.py
@@ -1,11 +1,50 @@
 # Day 18: Like a GIF For Your Yard
 import itertools
 
+DELTAS = set(itertools.product({-1, 0, 1}, repeat=2)) - {(0,0)}
+
 def parse_input(in_file):
     with open(in_file) as input_file:
         light_lines = input_file.readlines()
-
+    global DIMENSION
+    DIMENSION = len(light_lines)
     return {(row, column) for row, line in enumerate(light_lines) for column, light in enumerate(line) if light == '#' }
 
 
+def add_coordinates(tuple_1, tuple_2):
+    return tuple(one + two for one, two in zip(tuple_1, tuple_2))
+
+
+def neighbors(in_coordinate):
+    return set(add_coordinates(in_coordinate, delta) for delta in DELTAS
+            if 0 <= add_coordinates(in_coordinate, delta)[0] < DIMENSION and 0 <= add_coordinates(in_coordinate, delta)[1] < DIMENSION)
+
+
+def turn_on_the_lights(in_lights_on):
+    out_lights_on = set()
+    relevant_off_lights = set()
+    for light in in_lights_on:
+        relevant_off_lights |= neighbors(light)
+    relevant_off_lights -= in_lights_on
+
+    for light in in_lights_on | relevant_off_lights:
+        count_neighbors = sum(neighbor in in_lights_on for neighbor in neighbors(light))
+        if ((light in in_lights_on and count_neighbors in {2, 3}) or
+             light in relevant_off_lights and count_neighbors == 3):
+            out_lights_on.add(light)
+    return out_lights_on
+
+
+def lights_on_after_cycles(in_lights_on, cycles):
+    lights_on = set(in_lights_on)
+    for _ in range(cycles):
+        lights_on = turn_on_the_lights(lights_on)
+    return len(lights_on)
+
+
 all_lights = parse_input('inputs/input18')
+cycles = 100
+
+part_1 = lights_on_after_cycles(all_lights, cycles)
+print(f'After {cycles} cycles, there are {part_1} lights left on!')
+# After 100 cycles, there are 814 lights left on!
