0e55f61401352702f35e658b4e2bf0b19a8584b5
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-06 09:24:17 +0100

    [day17] clean up and add nice printouts

--- 17.py
+++ 17.py
@@ -11,19 +11,16 @@ LITERS = 150
 
 all_valid_combos = [combo for n in range(2, len(CONTAINERS)+1) for combo in itertools.combinations(CONTAINERS, n) if sum(combo) == LITERS]
 part_1 = len(all_valid_combos)
-print(part_1)
+
+print(f'There are {part_1} possible container combinations to fit {LITERS} liters of eggnog!')
+# There are 1638 possible container combinations to fit 150 liters of eggnog!
+
 
 # because of the way all_valid_combos is generated, it is actually already sorted on combo length
 min_num_of_containers_used = len(all_valid_combos[0])
 
 combos_with_min_number_of_containers = list(filter(lambda x: len(x) == min_num_of_containers_used, all_valid_combos))
+part_2 = len(combos_with_min_number_of_containers)
 
-print(len(combos_with_min_number_of_containers))
-quit()
-
-print(all_valid_combos[0], all_valid_combos[15], all_valid_combos[-1])
-
-min_num_of_containers_used = min(len(valid_combo) for valid_combo in all_valid_combos)
-combos_with_min_number_of_containers = len([valid_combo for valid_combo in all_valid_combos if len(valid_combo) == min_num_of_containers_used])
-part_2 = combos_with_min_number_of_containers
-print(part_2)
+print(f'There are {part_2} possible combinations that use a minimal number of containers ({min_num_of_containers_used})!')
+# There are 17 possible combinations that use a minimal number of containers (4)!
