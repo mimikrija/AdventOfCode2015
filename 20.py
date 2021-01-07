8f4bd9c4055530c87bbe03326344b56cc10a16e8
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-07 12:34:17 +0100

    [day20] part 1
    I wanted to do something "smarter", like adding up all divisors of a house
    and then looking for the lowest house number whose sum of divisors is greater
    than or equal to my_input//10, but that was very slow, so I had to use this
    approach which I found on Reddit

--- 20.py
+++ 20.py
@@ -1,3 +1,19 @@
 # Day 20: Infinite Elves and Infinite Houses
 
 my_input = 36000000
+max_elves = my_input // 10
+
+# every house gets 10 gifts from elf no.1
+house_gifts = [10 for _ in range(max_elves+1)]
+
+# after that, every n-th (or should I say elf-th) house gets n*10 gifts
+for elf in range(2, max_elves+1):
+    for house_num in range(elf, max_elves+1, elf):
+        house_gifts[house_num] += elf*10
+
+for n, gifts in enumerate(house_gifts):
+    if gifts >= my_input:
+        print(f'House number {n} has {gifts} gifts!')
+        break
+
+# House number 831600 has 36902400 gifts!
