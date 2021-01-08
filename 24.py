5316b656868175ca7919b26da118c1d82e53bde7
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-08 20:15:06 +0100

    [day24] both parts, assuming rest is splittable
    inspired by Reddit solution megathread, I implemented this where we
    assume (based on the input data) that once we find the smallest group,
    the rest will be splittable into remaining groups.
    
    the fact that we use itertools.permutations on a sorted list makes sure
    that whatever the first found smallest group is, it satisfies the
    "smallest cosmic entanglement" condition, ie. that this is not only the
    smallest group size, but the elements in the group are also smallest
    possible.
    
    to do: add check which proves that the remaining numbers WILL split into
    equal sum groups.

--- 24.py
+++ 24.py
@@ -1,5 +1,10 @@
 # Day 24: It Hangs in the Balance
 
+import itertools
+from operator import mul
+from functools import reduce
+
+
 def parse_input(in_file):
     with open(in_file) as input_file:
         inputs = input_file.readlines()
@@ -7,4 +12,21 @@ def parse_input(in_file):
     return list(map(int, inputs))
 
 
+def find_smallest_group(weights, groups):
+    target_weight = sum(weights) // groups
+    for group_size in range(1, len(weights)-1):
+        combos = itertools.permutations(weights, group_size)
+        for combo in combos:
+            if sum(combo) == target_weight:
+                return combo
+
+
 weights = parse_input('inputs/input24')
+
+
+part_1, part_2 = (reduce(mul,find_smallest_group(weights, groups)) for groups in {3, 4})
+print(f'The cosmic entanglement value for the smallest group if divided into 3 groups is {part_1}!')
+print(f'The cosmic entanglement value for the smallest group if divided into 4 groups is {part_2}!')
+
+# The cosmic entanglement value for the smallest group if divided into 3 groups is 10439961859!
+# The cosmic entanglement value for the smallest group if divided into 4 groups is 72050269!
