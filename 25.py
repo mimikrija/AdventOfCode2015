3ff0f0eb156f81205908ec78124cba4ee2ee1e5d
Maja Gacesa <maja.gacesa@gmail.com>
2021-01-10 17:11:11 +0100

    [day25] simplify code

--- 25.py
+++ 25.py
@@ -1,37 +1,18 @@
 # Day 25: Let It Snow
 
-from collections import namedtuple
-
 input_row, input_column = 2947, 3029
 
-def generate_code(previous_value):
-    return (previous_value * 252533) % 33554393
-
-def get_previous_position(row, column):
-    if column == 1:
-        return 1, row - 1
-    else:
-        return row + 1, column - 1
-
-def next_position(row, column):
-    if row == 1:
-        return (column + 1, 1)
-    else:
-        return (row - 1, column + 1)
-
-Code = namedtuple('Code', ['row', 'column', 'value'])
-code_table = [Code(1, 1, 20151125)]
+def generate_code(member):
+    value = 20151125
+    for _ in range(member-1):
+        value = (value * 252533) % 33554393
+    return value
 
 
-current_position = (1,1)
-while True:
-    previous_row, previous_column, previous_value = code_table[-1]
-    current_row, current_column = next_position(previous_row, previous_column)
-    value = generate_code(previous_value)
-    if current_row == input_row and current_column == input_column:
-        print(value)
-        break
-    else:
-        code_table.append(Code(current_row, current_column, value))
+def which_member(row, column):
+    return sum(n for n in range(1, row + column - 1)) + column
 
-# 19980801
+
+part_1 = generate_code(which_member(input_row, input_column))
+print(f'I give the machine code {part_1}!')
+# I give the machine code 19980801!
